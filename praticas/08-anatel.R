9af44849abf367b17442963feeb2922366f62dd7
Julio Trecenti <julio.trecenti@gmail.com>
2023-06-28 14:55:31 -0300

    exemplo anatel arrumado

--- praticas/08-anatel.R
+++ praticas/08-anatel.R
@@ -63,15 +63,28 @@ r_anatel |>
 ## paginação
 ## TODO: Descobrir como fazer a paginação nesse caso.
 
+lista_itens <- r_anatel |>
+  xml2::read_html() |>
+  xml2::xml_find_first("//script") |>
+  xml2::xml_text() |>
+  stringr::str_extract("(?<=vetor_codigos\\[1\\] = \\[)[^\\]]+") |>
+  stringr::str_split(",") |>
+  purrr::pluck(1)
+
+pagina <- 2
+itens_pag <- lista_itens[(((pagina-1)*20)+1):(min(pagina*20, length(lista_itens)))] |>
+  paste(collapse = ", ")
+
+
 u_paginacao <- "https://anatel.gov.br/biblioteca/index.asp"
 q_paginacao <- list(
   "modo_busca" = "legislacao",
   "veio_de" = "paginacao",
-  "pagina" = "|2",
+  "pagina" = glue::glue("|{pagina}"),
   "indice" = "|1",
   "submeteu" = "legislacao",
   "content" = "resultado",
-  "vetor_pag" = "|[2]39860.2, 38341.2, 37135.2, 31868.2, 21470.2, 17445.2, 24489.2, 20156.2, 23220.2",
+  "vetor_pag" = glue::glue("'|[{pagina}]{itens_pag}'"),
   "Servidor" = "1",
   "iSrvCombo" = "",
   "iBanner" = "0",
@@ -87,19 +100,39 @@ q_paginacao <- list(
 r_paginacao <- httr::GET(
   u_paginacao,
   query = q_paginacao,
-  httr::write_disk("output/pag.html", overwrite = TRUE),
-  httr::user_agent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36")
+  httr::write_disk("output/pag.html", overwrite = TRUE)
 )
-
-httr::content(r_paginacao, "text") |>
-  cat()
-
 
 ## parse da página de resultados
 
 tabela <- xml2::read_html("output/anatel.html", encoding = "UTF-8") |>
-  xml2::xml_find_first("//table[@class='max_width']")
+  xml2::xml_find_all("//table[@class='max_width' and contains(@style,'border-spacing: 1px')]")
 
+da_anatel_html_table <- tabela |>
+  rvest::html_table(header = FALSE)
+
+da_anatel_tidy <- da_anatel_html_table |>
+  dplyr::select(1,2) |>
+  dplyr::mutate(
+    numero = ifelse(stringr::str_detect(X1, "^[0-9]+$"), X1, NA)
+  ) |>
+  tidyr::fill(numero) |>
+  ## retirar os números e valores vazios
+  dplyr::filter(
+    !stringr::str_detect(X1, "^[0-9]+$"),
+    !is.na(X2),
+    !X2 %in% c("")
+  ) |>
+  ## tinha um valor com "Texto Integral" com "I"
+  dplyr::mutate(X1 = tolower(X1)) |>
+  ## pivota a base
+  tidyr::pivot_wider(names_from = X1, values_from = X2) |>
+  janitor::clean_names()
+
+## pag
+
+tabela <- xml2::read_html("output/pag.html", encoding = "UTF-8") |>
+  xml2::xml_find_first("//table[@class='max_width' and contains(@style,'border-spacing: 1px')]")
 
 da_anatel_html_table <- tabela |>
   rvest::html_table(header = FALSE)
